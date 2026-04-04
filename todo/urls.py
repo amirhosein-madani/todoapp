ec7733f4321edeefed86eb516d66793a4e99b8d9
amir <amirmadani901@gmail.com>
2026-04-04 18:33:49 +0330

    remove

--- todo/urls.py
+++ todo/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("add-task/" , AddTaskView.as_view() , name  = "add_task"),
     path("complete-task/<int:pk>/" , CompleteTaskView.as_view() , name = "complete_task"),
     path("undone-task/<int:pk>/", UndoneTaskView.as_view() ,name = "undone_task"),
-    path('api/v1/' , include('todo.api.v1.urls'))
+
 ]
